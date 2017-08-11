1202b5d87677509a96005cd29234e11c0f78c4c6
lgdamy <lgdamy@gmail.com>
2017-08-10 21:03:31 -0300

    https://www.hackerrank.com/challenges/java-2d-array/problem

---
+++ Java/Java 2D Array.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int arr[][] = new int[6][6];
+        for(int i=0; i < 6; i++){
+            for(int j=0; j < 6; j++){
+                arr[i][j] = in.nextInt();
+            }
+        }
+        int maiorSoma = 0;
+        for (int i = 0; i<4; i++) {
+            for ( int j = 0; j<4; j++) {
+                int sominha = 0;
+                for (int k = i; k < i+3; k++) {
+                    for (int l = j; l < j+3; l++) {
+                        if(!(l==j+1&&k!=i+1)) {
+                            sominha+=arr[k][l];
+                        }
+                    }
+                }
+                maiorSoma = sominha>maiorSoma?sominha:maiorSoma;
+            }
+        }
+        System.out.println(maiorSoma);
+    }
+}
