a8814709857fd586ba63e41ac8c390a7842a7d32
lgdamy <lgdamy@gmail.com>
2017-08-10 22:37:39 -0300

    Dimension Repair

--- Java/Java 2D Array.java
+++ Java/Java 2D Array.java
@@ -14,14 +14,14 @@ public class Solution {
                 arr[i][j] = in.nextInt();
             }
         }
-        int maiorSoma = 0;
+        int maiorSoma = Integer.MIN_VALUE;
         for (int i = 0; i<4; i++) {
             for ( int j = 0; j<4; j++) {
                 int sominha = 0;
                 for (int k = i; k < i+3; k++) {
                     for (int l = j; l < j+3; l++) {
                         if(!(l==j+1&&k!=i+1)) {
-                            sominha+=arr[k][l];
+                            sominha+=arr[l][k];
                         }
                     }
                 }
