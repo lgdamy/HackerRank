cee8a38548b3399e22b200941b3a19159e7060a5
lgdamy <lgdamy@gmail.com>
2017-09-04 23:32:12 -0300

    https://www.hackerrank.com/challenges/java-sort/problem

---
+++ Java/Java Sort.java
@@ -0,0 +1,57 @@
+class Student{
+   private int id;
+   private String fname;
+   private double cgpa;
+   public Student(int id, String fname, double cgpa) {
+      super();
+      this.id = id;
+      this.fname = fname;
+      this.cgpa = cgpa;
+   }
+   public int getId() {
+      return id;
+   }
+   public String getFname() {
+      return fname;
+   }
+   public double getCgpa() {
+      return cgpa;
+   }
+}
+
+class StudentSort implements Comparator<Student>{
+    public int compare(Student a, Student b){
+        if (a.getCgpa() != b.getCgpa()) {
+            return (int)(b.getCgpa()*100-a.getCgpa()*100);
+        } else if (!a.getFname().equals(b.getFname())) {
+            return a.getFname().compareTo(b.getFname());
+        } else {
+            return b.getId()-a.getId();
+        }
+    }
+    
+}
+public class Solution
+{
+   public static void main(String[] args){
+      Scanner in = new Scanner(System.in);
+      int testCases = Integer.parseInt(in.nextLine());
+      
+      List<Student> studentList = new ArrayList<Student>();
+      while(testCases>0){
+         int id = in.nextInt();
+         String fname = in.next();
+         double cgpa = in.nextDouble();
+         
+         Student st = new Student(id, fname, cgpa);
+         studentList.add(st);
+         
+         testCases--;
+      }
+       Collections.sort(studentList,new StudentSort());
+      
+         for(Student st: studentList){
+         System.out.println(st.getFname());
+      }
+   }
+}
